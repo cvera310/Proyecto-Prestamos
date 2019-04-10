98ff91177bc986429307dce0e9eb9cb768e23182
christian Vera <christian-vera@outlook.com>
2019-04-10 17:13:37 -0400

    Prueba 2

--- Prestamos/Prestamos/Program.cs
+++ Prestamos/Prestamos/Program.cs
@@ -11,7 +11,7 @@ namespace Prestamos
         static void Main(string[] args)
         {
             Console.WriteLine("Hola Mundo");
-
+            Console.WriteLine("Hola Mundo");
         }
     }
 }

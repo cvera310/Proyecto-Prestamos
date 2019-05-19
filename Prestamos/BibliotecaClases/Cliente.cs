6aa5bd2e6b1b85b2e0d3c76778e5bedbe5f3bac9
christian Vera <linuxvera@gmail.com>
2019-05-19 15:51:29 -0400

    Actualizacion cliente y frmEvaluacionCreditos

--- Prestamos/BibliotecaClases/Cliente.cs
+++ Prestamos/BibliotecaClases/Cliente.cs
@@ -28,7 +28,7 @@ namespace BibliotecaClases
         public String RazonSocial { get; set; }
 
 
-        public String Nacimiento { get; set; }
+        public DateTime Nacimiento { get; set; }
         public String Telefono { get; set; }
         public String Direccion { get; set; }
         public String Foto { get; set; }
@@ -36,6 +36,9 @@ namespace BibliotecaClases
         public String LugarTrabajo { get; set; }
         public int AntiguedadLaboral { get; set; }
         public Boolean Informconf { get; set; }
+        public TipoDocumento tipo_documento { get; set; }
+        public Sexo sexo { get; set; }
+
 
         public static List<Cliente> ListaCliente = new List<Cliente>();
 
